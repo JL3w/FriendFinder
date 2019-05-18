383d403464bce1641e207ffce00c954b2244c108
J Lew <jtl@metricwise.net>
2019-05-18 02:18:35 -0400

    home.html boiler plate fix apiroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,12 @@
-var path = require("path");
+var friends = require("../data/friends");
 
 module.exports = function(app) {
     // send our friend data stored in friends.js when a get on api/friends is performed
     app.get("/api/friends", function(req, res) {
-        res.sendFile(path.join(__dirname, "../data/friends.js"))
+        res.json(friends);
     });
     // on survey submission receive user survey data(req) as a var for calcin, calculate best "friend", send match data (res), save data back to friends.js
     app.post("api/friends", function(req, res) {
-        // 
+        // logics
     })
 }
