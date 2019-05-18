54af2b5ef9bfd4006f9705eab97da7e0233e203b
J Lew <jtl@metricwise.net>
2019-05-18 01:17:59 -0400

    add app.get route, psuedo-code app.post route

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+module.exports = function(app) {
+    // send our friend data stored in friends.js when a get on api/friends is performed
+    app.get("/api/friends", function(req, res) {
+        res.sendFile(path.join(__dirname, "../data/friends.js"))
+    });
+    // on survey submission receive user survey data(req) as a var for calcin, calculate best "friend", send match data (res), save data back to friends.js
+    app.post("api/friends", function(req, res) {
+        // 
+    })
+}
